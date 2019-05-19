2931c08e0599294ac46a848f7da49c632ffcdb07
kote6725@gmail.com <kote6725@gmail.com>
2019-05-19 18:30:18 +0300

    Добавьте файлы проекта.

---
+++ lab4Cpp/lab4Cpp.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using word_arr = std::vector<std::string>;
+
+int main()
+{
+	word_arr words;
+	std::string str = "dgd sgdf sgfgsd sdfg sfg sfgsgfsgfsdrg fse";
+	std::stringstream stream(str);
+	std::string temp;
+	while (stream >> temp)
+	{
+		std::cout << temp << std::endl;
+		words.push_back(temp);
+	}
+}
